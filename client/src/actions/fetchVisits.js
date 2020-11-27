be3f9560fe3c9fb6c5df9c911765b9d518732ed3
wickbushong <wickbushong@gmail.com>
2020-11-27 14:15:53 -0600

    Set up dummy fetch visits action.

--- client/src/actions/fetchVisits.js
+++ client/src/actions/fetchVisits.js
@@ -1,6 +1,8 @@
 export const fetchVisits = () => {
     return (dispatch) => {
         dispatch({ type: 'LOADING_VISITS' });
-        fetch()
+        fetch('http://localhost:3001/api/v1/visits')
+            .then(response => response.json())
+                .then(visits => dispatch({ type: 'ADD_VISITS' }))
     }
 }
