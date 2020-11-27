f78521543eb66dae5454d84b7eddda95ab537eb2
wickbushong <wickbushong@gmail.com>
2020-11-27 16:34:49 -0600

    Create auth controller and create action.

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::AuthController < ApplicationController
+    skip_before_action :authorized, only: %i[create]
+
+    def create
+        @business = Business.find_by(email: business_login_params[:email])
+        #User#authenticate comes from BCrypt
+        if @business && @business.authenticate(business_login_params[:password])
+          # encode token comes from ApplicationController
+          token = encode_token({ business_id: @business.id })
+          render json: { business: BusinessSerializer.new(@business), jwt: token }, status: :accepted
+        else
+          render json: { message: 'Invalid email or password' }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def business_login_params
+        params.require(:business).permit(:email, :password)
+    end
+end
