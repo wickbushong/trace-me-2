5a2b6a4bf449197eba50e2de7ebbf6da564ac882
wickbushong <wickbushong@gmail.com>
2020-11-27 17:51:25 -0600

    create bare bones of log in page.

---
+++ client/src/containers/LogInPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { connect } from 'react-redux'
+
+const LogInPage = props => {
+    // destructuring props 
+    return (
+        <div>
+            THIS IS THE LOG IN PAGE
+
+        </div>
+    )
+}
+
+export default LogInPage
--- client/src/containers/SignUpPage.js
+++ client/src/containers/SignUpPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { connect } from 'react-redux'
+// import { connect } from 'react-redux'
 
 const SignUpPage = props => {
     // props destructuring
     return (
         <div>
             THIS IS THE SIGN UP PAGE
+            SIGN UP FORM GOES HERE
         </div>
     )
 }
