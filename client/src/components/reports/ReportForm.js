ff209b9c29ca07030d300e9cadb238375d3f6601
wickbushong <wickbushong@gmail.com>
2020-11-30 09:38:09 -0600

    Give ReportForm controlled inputs.

--- client/src/components/reports/ReportForm.js
+++ client/src/components/reports/ReportForm.js
@@ -1,20 +1,90 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
 // var DateTimeField = require('react-bootstrap-datetimepicker');
 // import {DateTime} from 'react-datetime-bootstrap'
 
 
 class ReportForm extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            date: ''
+        }
+    }
+
+    handleOnChange = event => {
+        this.setState(prevState => {
+            return {
+            ...prevState,
+            [event.target.name]: event.target.value
+        }})
+    }
+
+    handleOnSubmit = event => {
+        event.preventDefault()
+        console.log(this.state)
+        this.setState({
+            firstName: '',
+            lastName: '',
+            email: '',
+            date: ''
+        })
+    }
 
     render() {
         return (
-            <Form>
-                
-                <Form.Group controlId="formDate">
-                    <Form.Control type="date" id="date-input" className="form-control datetimepicker-input" />
+            <Form onSubmit={this.handleOnSubmit}>
+                <label htmlFor="formName">Name</label>                    
+                <InputGroup className="mb-3" id="formName">
+                    <Form.Control 
+                        type="text" 
+                        placeholder="first"
+                        name="firstName"
+                        required
+                        value={this.state.firstName}
+                        onChange={this.handleOnChange}
+                    />
+                    <Form.Control 
+                        type="text"
+                        placeholder="last"
+                        name="lastName"
+                        required
+                        value={this.state.lastName}
+                        onChange={this.handleOnChange}
+                    />
+                </InputGroup>
+
+                <Form.Group controlId="formEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        placeholder="address@domain.com"
+                        required 
+                        value={this.state.email}
+                        onChange={this.handleOnChange}
+                    />
                 </Form.Group>
-                <Button type="submit" variant="info" block >SUBMIT REPORT</Button>
+
+                <Form.Group controlId="formDate">
+                    <Form.Label>Date Test Was Administered</Form.Label>
+                    <Form.Control 
+                        type="date"
+                        className="form-control datetimepicker-input" 
+                        name="date"
+                        value={this.state.date}
+                        onChange={this.handleOnChange}
+                    />
+                </Form.Group>
+
+                <Form.Group>
+                    <Button type="submit" variant="info" block >SUBMIT REPORT</Button>
+                </Form.Group>
             </Form>
         )
     }
