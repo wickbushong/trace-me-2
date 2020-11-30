ac238b82ad64c9b0a1bce2d2750c7be28993b5ab
wickbushong <wickbushong@gmail.com>
2020-11-30 09:10:08 -0600

    Figuring out date time picker in react bootstrap.

--- client/src/components/reports/ReportForm.js
+++ client/src/components/reports/ReportForm.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+// var DateTimeField = require('react-bootstrap-datetimepicker');
+// import {DateTime} from 'react-datetime-bootstrap'
 
 
 class ReportForm extends React.Component {
@@ -9,6 +11,9 @@ class ReportForm extends React.Component {
         return (
             <Form>
                 
+                <Form.Group controlId="formDate">
+                    <Form.Control type="date" id="date-input" className="form-control datetimepicker-input" />
+                </Form.Group>
                 <Button type="submit" variant="info" block >SUBMIT REPORT</Button>
             </Form>
         )
