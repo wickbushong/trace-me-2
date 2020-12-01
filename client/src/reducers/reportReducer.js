055ad726b9524ef262dbfc65739b70ccfe9e50dd
wickbushong <wickbushong@gmail.com>
2020-12-01 16:57:02 -0600

    Add business logout case to report reducer.

--- client/src/reducers/reportReducer.js
+++ client/src/reducers/reportReducer.js
@@ -3,6 +3,8 @@ export const reportReducer = (state = {}, action) => {
     switch (action.type) {
         case 'ADD_REPORT':
             return action.payload;
+        case 'LOGOUT_BUSINESS':
+            return {}
         default:
             return state;
     }
