bd494dc05d9c566d89045230264c35135b723fc0
wickbushong <wickbushong@gmail.com>
2020-12-03 15:24:55 -0600

    Add error to sign up page.

--- client/src/containers/SignUpPage.js
+++ client/src/containers/SignUpPage.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
+
 import SignUpForm from '../components/auth/SignUpForm'
+import Error from '../components/Error'
+
 import addBusiness from '../actions/businesses/addBusiness'
 
 const SignUpPage = props => {
-    let {addBusiness} = props
+    let {addBusiness, errors} = props
     return (
         <div>
+            <Error errors={errors} />
             <h1>let's get you signed up</h1>
             <p>all we need is some basic info about your business - don't worry, we'll keep it to ourselves</p>
             <SignUpForm addBusiness={addBusiness} />
@@ -14,4 +18,8 @@ const SignUpPage = props => {
     )
 }
 
-export default connect(null, {addBusiness})(SignUpPage)
+const mapStateToProps = ({errors}) => {
+    return {errors}
+}
+
+export default connect(mapStateToProps, {addBusiness})(SignUpPage)
