45c0c9114819f76686e1aab3f2cf00fe14378af8
wickbushong <wickbushong@gmail.com>
2020-12-04 14:35:29 -0600

    change Error close button prop to className.

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -19,7 +19,7 @@ class Error extends React.PureComponent {
                 { !!keys.length ? (
                     <Alert variant='danger' className="container">
                         {keys.map(key => <span key={key}>{key} - {errors[key]}</span>)}
-                        <button type="button" class="close" aria-label="Close" onClick={() => this.props.resetErrors()}>
+                        <button type="button" className="close" aria-label="Close" onClick={() => this.props.resetErrors()}>
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </Alert>
