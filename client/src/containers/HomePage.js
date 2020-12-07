ab942b233bb21f190947a4168ff6d9ce93a1af45
wickbushong <wickbushong@gmail.com>
2020-12-07 15:19:20 -0600

    Convert HomePage to functional component.

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -1,24 +1,23 @@
 import React from 'react'
+
 import { NavLink } from 'react-router-dom';
+
 import Button from 'react-bootstrap/Button'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 
-class HomePage extends React.PureComponent {
-
-    render() {
-        return (
-            <Jumbotron>
-                <h1>Welcome to TraceMe</h1>
-                <h3>Contact tracing made easy.</h3>
-                <p>
-                    Sign up and start keeping track of your comers and goers.
-                </p>
-                <p>
-                    <Button as={NavLink} to="/signup" variant="info">Get Started</Button>
-                </p>
-            </Jumbotron>
-        )
-    }
+const HomePage = () => {
+    return (
+        <Jumbotron>
+            <h1>Welcome to TraceMe</h1>
+            <h3>Contact tracing made easy.</h3>
+            <p>
+                Sign up and start keeping track of your comers and goers.
+            </p>
+            <p>
+                <Button as={NavLink} to="/signup" variant="info">Get Started</Button>
+            </p>
+        </Jumbotron>
+    )
 }
 
 export default HomePage
