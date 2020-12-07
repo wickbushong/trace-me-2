6200e4e34a9a07cbe537a771c4270b35a2c584bf
wickbushong <wickbushong@gmail.com>
2020-12-07 15:30:30 -0600

    Comvert to functional component.

--- client/src/navigation/AnonymousNav.js
+++ client/src/navigation/AnonymousNav.js
@@ -1,18 +1,18 @@
 import React from 'react'
+
 import { NavLink } from 'react-router-dom';
+
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button'
 
-class AnonymousNav extends React.PureComponent {
-    render() {
-        return (
-            <Nav className="container-fluid">
-                <Nav.Link as={NavLink} to="/login">Log In</Nav.Link>
-                <Nav.Link as={NavLink} to="/signup">Sign Up</Nav.Link>
-                <Button variant="outline-danger" className="ml-auto" as={NavLink} to="/report">Report</Button>
-            </Nav>
-        )
-    }
+const AnonymousNav = () => {
+    return (
+        <Nav className="container-fluid">
+            <Nav.Link as={NavLink} to="/login">Log In</Nav.Link>
+            <Nav.Link as={NavLink} to="/signup">Sign Up</Nav.Link>
+            <Button variant="outline-danger" className="ml-auto" as={NavLink} to="/report">Report</Button>
+        </Nav>
+    )
 }
 
 export default AnonymousNav
