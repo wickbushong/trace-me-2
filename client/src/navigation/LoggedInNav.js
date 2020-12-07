25bef645c154b61ac2636a89eda568ff07229b06
wickbushong <wickbushong@gmail.com>
2020-12-07 11:52:56 -0600

    Reformat nav bar.

--- client/src/navigation/LoggedInNav.js
+++ client/src/navigation/LoggedInNav.js
@@ -6,10 +6,10 @@ import Nav from 'react-bootstrap/Nav'
 class LoggedInNav extends React.PureComponent {
     render() {
         return (
-            <Nav className="container-fluid">
+            <Nav className="ml-auto">
                 <Nav.Link as={NavLink} to="/visits">Visitor Log</Nav.Link>
                 <Nav.Link as={NavLink} to="/report">Report</Nav.Link>
-                <Nav.Link className="ml-auto" as={NavLink} to='/logout' onClick={this.props.logOut} >Log Out</Nav.Link>
+                <Nav.Link as={NavLink} to='/logout' onClick={this.props.logOut} >Log Out</Nav.Link>
             </Nav>
         )
     }
