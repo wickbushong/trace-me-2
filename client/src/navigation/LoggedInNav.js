e10fbfa11737cac7d299b95256976e81c7c1eefa
wickbushong <wickbushong@gmail.com>
2020-12-07 15:31:35 -0600

    Convert to functional component.

--- client/src/navigation/LoggedInNav.js
+++ client/src/navigation/LoggedInNav.js
@@ -1,18 +1,17 @@
 import React from 'react'
+
 import { NavLink } from 'react-router-dom';
+
 import Nav from 'react-bootstrap/Nav'
 
-
-class LoggedInNav extends React.PureComponent {
-    render() {
-        return (
-            <Nav className="ml-auto">
-                <Nav.Link as={NavLink} to="/visits">Visitor Log</Nav.Link>
-                <Nav.Link as={NavLink} to="/report">Report</Nav.Link>
-                <Nav.Link as={NavLink} to='/logout' onClick={this.props.logOut} >Log Out</Nav.Link>
-            </Nav>
-        )
-    }
+const LoggedInNav = () => {
+    return (
+        <Nav className="ml-auto">
+            <Nav.Link as={NavLink} to="/visits">Visitor Log</Nav.Link>
+            <Nav.Link as={NavLink} to="/report">Report</Nav.Link>
+            <Nav.Link as={NavLink} to='/logout' onClick={this.props.logOut} >Log Out</Nav.Link>
+        </Nav>
+    )
 }
 
 export default LoggedInNav
