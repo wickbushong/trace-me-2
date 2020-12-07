fc7306fc9a1c00afc471454279bb87238ad00faa
wickbushong <wickbushong@gmail.com>
2020-12-07 15:48:51 -0600

    Remove console log.

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,5 +1,4 @@
 export const errorReducer = (state = {}, action) => {
-    console.log(action)
     switch (action.type) {
         case 'SERVER_ERROR':
             return {serverError: action.payload};
