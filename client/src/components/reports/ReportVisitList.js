900053c71b6d1885aa79ee6fdf8ff789b0500338
wickbushong <wickbushong@gmail.com>
2020-12-07 14:54:10 -0600

    Change ReportVisitList to functional component.

--- client/src/components/reports/ReportVisitList.js
+++ client/src/components/reports/ReportVisitList.js
@@ -4,25 +4,22 @@ import Accordion from 'react-bootstrap/Accordion'
 
 import ReportVisit from './ReportVisit'
 
-class ReportVisitList extends React.PureComponent {
+const ReportVisitList = ({report}) => {
 
-    renderVisits = flaggedVisits => {
+    const renderVisits = flaggedVisits => {
         return flaggedVisits.map(visit => {
             return <ReportVisit key={visit.id} visit={visit} />
         })
     }
-
-    render() {
-        const {report} = this.props
-        return (
-            <>
-                <h1 className="text-center">FLAGGED VISITS</h1>
-                <Accordion>
-                    {this.renderVisits(report.flag_visits)}
-                </Accordion>
-            </>
-        )
-    }
+    
+    return (
+        <>
+            <h1 className="text-center">FLAGGED VISITS</h1>
+            <Accordion>
+                {renderVisits(report.flag_visits)}
+            </Accordion>
+        </>
+    )
 }
 
 export default ReportVisitList
