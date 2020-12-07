617b2133d6a065e40defd8f5650cfd495fff3203
wickbushong <wickbushong@gmail.com>
2020-12-07 15:04:54 -0600

    Convert ActiveVisit to functional component.

--- client/src/components/visits/ActiveVisit.js
+++ client/src/components/visits/ActiveVisit.js
@@ -3,19 +3,15 @@ import React from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Button from 'react-bootstrap/Button'
 
-class ActiveVisit extends React.Component {
+const ActiveVisit = ({visit, handleCheckOut}) => {
 
-    handleOnClick = event => {
-        this.props.handleCheckOut(this.props.visit.id)
-    }
-    
-    render() {
-        const {visit} = this.props
-        return (
-            <ListGroup.Item key={visit.id} >{visit.user.first_name} {visit.user.last_name} <Button variant="info" size="sm" className="float-right" onClick={this.handleOnClick} >Check Out</Button></ListGroup.Item>
-        )
+    const handleOnClick = () => {
+        handleCheckOut(visit.id)
     }
 
+    return (
+        <ListGroup.Item key={visit.id} >{visit.user.first_name} {visit.user.last_name} <Button variant="info" size="sm" className="float-right" onClick={handleOnClick} >Check Out</Button></ListGroup.Item>
+    )
 }
 
 export default ActiveVisit
