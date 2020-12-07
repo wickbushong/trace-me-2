b375d3a70baf32d609d4d84ba21b7ba644edb4bf
wickbushong <wickbushong@gmail.com>
2020-12-07 14:48:26 -0600

    Make Error functional component.

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -5,36 +5,25 @@ import Alert from 'react-bootstrap/Alert'
 
 import resetErrors from '../actions/resetErrors'
 
-class Error extends React.PureComponent {
-    
-    handleClose = () => {
-        this.props.resetErrors()
-    }
+const Error = ({errors, resetErrors}) => {
+    let keys = Object.keys(errors)
 
-    render() {
-        let {errors} = this.props
-        let keys = Object.keys(errors)
-        return (
-            <>
-                { !!keys.length ? (
-                    <Alert variant='danger' className="container">
-                        {keys.map(key => <span key={key}>{key} - {errors[key]}</span>)}
-                        <button type="button" className="close" aria-label="Close" onClick={() => this.props.resetErrors()}>
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </Alert>
-                ) : null }
-            </>
-        )
-    }
-
-
+    return (
+        <>
+            { !!keys.length ? (
+                <Alert variant='danger' className="container">
+                    {keys.map(key => <span key={key}>{key} - {errors[key]}</span>)}
+                    <button type="button" className="close" aria-label="Close" onClick={() => resetErrors()}>
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </Alert>
+            ) : null }
+        </>
+    )
 }
 
-const mapStateToProps = state => {
-    return {
-        errors: state.errors
-    }
+const mapStateToProps = ({errors}) => {
+    return {errors}
 }
 
 export default connect(mapStateToProps, {resetErrors})(Error)
