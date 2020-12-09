83ba0900ef1273560751d297750feeddceff6bea
wickbushong <wickbushong@gmail.com>
2020-12-09 13:27:15 -0600

    Slight changes in active visit rendering.

--- client/src/components/visits/ActiveVisit.js
+++ client/src/components/visits/ActiveVisit.js
@@ -4,13 +4,15 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import Button from 'react-bootstrap/Button'
 
 const ActiveVisit = ({visit, handleCheckOut}) => {
-
+    
     const handleOnClick = () => {
         handleCheckOut(visit.id)
     }
 
     return (
-        <ListGroup.Item key={visit.id} >{visit.user.first_name} {visit.user.last_name} <Button variant="info" size="sm" className="float-right" onClick={handleOnClick} >Check Out</Button></ListGroup.Item>
+        <ListGroup.Item key={visit.id}>
+            {visit.user.first_name} {visit.user.last_name}<Button variant="info" size="sm" className="float-right" onClick={handleOnClick}>Check Out</Button>
+        </ListGroup.Item>
     )
 }
 
