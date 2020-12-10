4d12ee9e01c74d0c75e5a43d2f915b0f65b791cc
wickbushong <wickbushong@gmail.com>
2020-12-10 15:42:04 -0600

    Setting up user sign up form and page.

---
+++ client/src/components/auth/UserSignUpForm.js
@@ -0,0 +1,131 @@
+import React from 'react'
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+class UserSignUpForm extends React.PureComponent {
+    onstructor() {
+        super()
+        this.state = {
+            firstName: '',
+            lastName: '',
+            phone: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+
+    handleOnChange = event => {
+        this.setState(prevState => {
+            return {
+            ...prevState,
+            [event.target.name]: event.target.value
+        }})
+    }
+
+    handleConfirm = event => {
+        event.preventDefault()
+        if (event.target.value !== this.state.password) {
+            event.target.classList.add("is-invalid")
+        } else {
+            event.target.classList.remove("is-invalid")
+            event.target.classList.add("is-valid")
+        }
+    }
+
+    passwordsMatch = () => {
+        return !!(this.state.password === this.state.confirmPassword)
+    }
+    
+    handleOnSubmit = event => {
+        event.preventDefault()
+        this.passwordsMatch() ? this.props.addUser(this.state) : document.getElementById("confirmPassword").focus();
+    }
+
+    render() {
+        return (
+            <Form onSubmit={this.handleOnSubmit}>
+                <Form.Group>
+                    <Form.Label>Name*</Form.Label>
+                    <Form.Control 
+                        type="text"
+                        name="name"
+                        placeholder="Enter business name"
+                        required
+                        value={this.state.name}
+                        onChange={this.handleOnChange}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Phone</Form.Label>
+                    <Form.Control 
+                        type="tel"
+                        name="phone"
+                        placeholder="Enter phone number"
+                        value={this.state.phone}
+                        onChange={this.handleOnChange}
+                    />
+                    <Form.Text className="text-muted">
+                        no dashes or parentheses please
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Location</Form.Label>
+                    <Form.Control 
+                        as="textarea" 
+                        rows={3}
+                        name="location"
+                        placeholder="Enter physical address"
+                        value={this.state.location}
+                        onChange={this.handleOnChange}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Email*</Form.Label>
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        placeholder="Enter email"
+                        required 
+                        value={this.state.email}
+                        onChange={this.handleOnChange}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password*</Form.Label>
+                    <Form.Control 
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="Enter password"
+                        required
+                        value={this.state.password}
+                        onChange={this.handleOnChange}
+                    />
+                </Form.Group>   
+                <Form.Group>
+                    <Form.Control 
+                        type="password"
+                        name="confirmPassword"
+                        id="confirmPassword"
+                        placeholder="Confirm password"
+                        className="invalid"
+                        required
+                        value={this.state.confirmPassword}
+                        onChange={this.handleOnChange}
+                        onKeyUp={this.handleConfirm}
+                    />
+                    <div class="invalid-feedback">
+                       passwords don't match
+                    </div>
+                </Form.Group>
+                <Button variant="info" type="submit">
+                    Sign Up
+                </Button>
+            </Form>
+        )
+    }
+}
+
+export default UserSignUpForm
---
+++ client/src/containers/UserSignUpPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import UserSignUpForm from '../components/auth/UserSignUpForm'
+
+import addUser from '../actions/businesses/addUser'
+
+const UserSignUpPage = ({addUser}) => {
+    return (
+        <div>
+            <h1>let's get you signed up</h1>
+            <p>all we need is some basic info - don't worry, we'll keep it to ourselves</p>
+            <UserSignUpForm addUser={addUser} />
+        </div>
+    )
+}
+
+export default connect(null, {addUser})(UserSignUpPage)
