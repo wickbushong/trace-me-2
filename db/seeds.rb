5195c379382457cd90c1b3d326a38040f91a52a3
wickbushong <wickbushong@gmail.com>
2020-12-10 18:16:36 -0600

    Adjust seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,52 +29,57 @@ User.create(
     first_name: "Wick",
     last_name: "Bushong",
     phone: "9876543210",
-    email: "wick@bushong.com"
+    email: "wick@bushong.com",
+    password: 'password'
 )
 
 User.create(
     first_name: "Margaret",
     last_name: "Long",
     phone: "2222222222",
-    email: "long.mkat@gmail.com"
+    email: "long.mkat@gmail.com",
+    password: 'password'
 )
 
 User.create(
     first_name: "Matt",
     last_name: "Logan",
     phone: "3333333333",
-    email: "matt@logan.com"
+    email: "matt@logan.com",
+    password: 'password'
 )
 
 User.create(
     first_name: "Robby",
     last_name: "Cowan",
     phone: "4444444444",
-    email: "robby@cowan.com"
+    email: "robby@cowan.com",
+    password: 'password'
 )
 
 User.create(
     first_name: "Haley",
     last_name: "Lyerly",
     phone: "5555555555",
-    email: "haley@lyerly.com"
+    email: "haley@lyerly.com",
+    password: 'password'
 )
 
-seconds = [86400, 172800, 259200, 345600, 432000, 518400, 604800, 691200, 777600, 864000, 950400, 1036800, 1123200, 
-1209600, 129600, 1382400]
-past16days = seconds.map do |s|
-    Time.now - s
-end
+# seconds = [86400, 172800, 259200, 345600, 432000, 518400, 604800, 691200, 777600, 864000, 950400, 1036800, 1123200, 
+# 1209600, 129600, 1382400]
+# past16days = seconds.map do |s|
+#     Time.now - s
+# end
 
-past16days.each do |d|
-    Visit.create(
-        user_id: 1,
-        business_id: 1,
-        time_in: d,
-        time_out: d + 3600,
-        party_size: 2
-    )
-end
+# past16days.each do |d|
+#     Visit.create(
+#         user_id: 1,
+#         business_id: 1,
+#         time_in: d,
+#         time_out: d + 3600,
+#         party_size: 2
+#     )
+# end
 
 # Visit.create(
 #     user_id: 1,
@@ -84,57 +89,57 @@ end
 #     party_size: 2
 # )
 
-Visit.create(
-    user_id: 4,
-    business_id: 1,
-    time_in: Time.now - 86400,
-    time_out: Time.now - 85400,
-    party_size: 2
-)
+# Visit.create(
+#     user_id: 4,
+#     business_id: 1,
+#     time_in: Time.now - 86400,
+#     time_out: Time.now - 85400,
+#     party_size: 2
+# )
 
-Visit.create(
-    user_id: 3,
-    business_id: 1,
-    time_in: Time.now - 88000,
-    time_out: Time.now - 86000,
-    party_size: 3
-)
+# Visit.create(
+#     user_id: 3,
+#     business_id: 1,
+#     time_in: Time.now - 88000,
+#     time_out: Time.now - 86000,
+#     party_size: 3
+# )
 
-Visit.create(
-    user_id: 2,
-    business_id: 1,
-    time_in: Time.now - 2400,
-    time_out: Time.now - 1200,
-    party_size: 5
-)
+# Visit.create(
+#     user_id: 2,
+#     business_id: 1,
+#     time_in: Time.now - 2400,
+#     time_out: Time.now - 1200,
+#     party_size: 5
+# )
 
-Visit.create(
-    user_id: 2,
-    business_id: 1,
-    time_in: Time.now - 346000,
-    time_out: Time.now - 345000,
-    party_size: 5
-)
+# Visit.create(
+#     user_id: 2,
+#     business_id: 1,
+#     time_in: Time.now - 346000,
+#     time_out: Time.now - 345000,
+#     party_size: 5
+# )
 
-Visit.create(
-    user_id: 3,
-    business_id: 1,
-    time_in: Time.now - 346000,
-    time_out: Time.now - 345000,
-    party_size: 5
-)
+# Visit.create(
+#     user_id: 3,
+#     business_id: 1,
+#     time_in: Time.now - 346000,
+#     time_out: Time.now - 345000,
+#     party_size: 5
+# )
 
-Visit.create(
-    user_id: 2,
-    business_id: 1,
-    time_in: Time.now,
-    party_size: 5
-)
+# Visit.create(
+#     user_id: 2,
+#     business_id: 1,
+#     time_in: Time.now,
+#     party_size: 5
+# )
 
-Visit.create(
-    user_id: 5,
-    business_id: 1,
-    time_in: Time.now - 10000,
-    time_out: Time.now - 9500,
-    party_size: 5
-)
+# Visit.create(
+#     user_id: 5,
+#     business_id: 1,
+#     time_in: Time.now - 10000,
+#     time_out: Time.now - 9500,
+#     party_size: 5
+# )
