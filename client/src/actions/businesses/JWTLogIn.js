0311aa2b8cca429fb9b0a5a9f50cb241c118dd88
wickbushong <wickbushong@gmail.com>
2020-12-18 08:51:28 -0600

    JWT auto login accounts for users and businesses seperately.

--- client/src/actions/businesses/JWTLogIn.js
+++ client/src/actions/businesses/JWTLogIn.js
@@ -16,8 +16,11 @@ export default function JWTLogIn() {
                     debugger
                     if (result.errors) {
                         dispatch({type: "LOGIN_ERROR", payload: result.errors})
-                    } else { 
-                        dispatch({type: "LOGIN_BUSINESS", payload: result})
+                    } else if (result.user) { 
+                        dispatch({type: "LOGIN_USER", payload: result.user})
+                        dispatch({type: "RESET_ERRORS"})
+                    } else {
+                        dispatch({type: "LOGIN_BUSINESS", payload: result.business})
                         dispatch({type: "RESET_ERRORS"})
                     }
                 })
