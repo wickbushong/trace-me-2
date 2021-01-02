26738e49ae0f5f385dcb3c707069aac179deceb5
wickbushong <wickbushong@gmail.com>
2021-01-02 13:56:39 -0600

    Remove anonymous report form button. Rearrange anonymous nav items.

--- client/src/navigation/AnonymousNav.js
+++ client/src/navigation/AnonymousNav.js
@@ -4,18 +4,17 @@ import { NavLink } from 'react-router-dom';
 
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import Button from 'react-bootstrap/Button'
 
 const AnonymousNav = () => {
     return (
-        <Nav className="container-fluid">
+        <Nav>
             <Nav.Link as={NavLink} to="/login">Log In</Nav.Link>
             <NavDropdown title="Sign Up" id="basic-nav-dropdown">
                 <NavDropdown.Item as={NavLink} to="/signup/business">For Businesses</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item as={NavLink} to="/signup/user">For Customers</NavDropdown.Item>
             </NavDropdown>
-            <Button variant="outline-danger" className="ml-auto" as={NavLink} to="/report">Report</Button>
+            {/* <Button variant="outline-danger" className="ml-auto" as={NavLink} to="/report">Report</Button> */}
         </Nav>
     )
 }
