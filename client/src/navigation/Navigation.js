598e737e74b170175aa8e62bc28c97efa8f18500
wickbushong <wickbushong@gmail.com>
2021-01-02 14:24:35 -0600

    Display users first name as brand icon in user nav.

--- client/src/navigation/Navigation.js
+++ client/src/navigation/Navigation.js
@@ -5,19 +5,23 @@ import Navbar from 'react-bootstrap/Navbar';
 import { connect } from 'react-redux';
 
 import BusinessNav from './BusinessNav'
+import UserNav from './UserNav'
 import AnonymousNav from './AnonymousNav';
 
 const Navigation = ({business, user, logOut}) => {
-    const entity = business.id || user.id ? business || user : null
-
+    const entity = business || user
+    const brandName = business.name || user.first_name || "TraceMe"
+    debugger
     return (
         <Navbar bg="dark" variant="dark" sticky="top" expand="md" >
             <Navbar.Brand as={NavLink} to="/">
-                {entity ? entity.name || entity.first_name : "TraceMe"}
+                {brandName}
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                { entity ? <BusinessNav logOut={logOut} entity={entity} /> : <AnonymousNav />}
+                { entity ? null : <AnonymousNav />}
+                { business.id ? <BusinessNav logOut={logOut} entity={entity} /> : null}
+                { user.id ? <UserNav logOut={logOut} entity={entity} /> : null}
             </Navbar.Collapse>
         </Navbar>
     )
